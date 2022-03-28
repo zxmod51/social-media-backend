4438a18bad811736c6736e9cb037a24ca9a8e88d
Murat Tekin <muteit02@hs-esslingen.de>
2022-03-29 00:04:31 +0200

    Setting Endpoint to request temperature of a weather API

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/getRtWetter', (req, res) => {
+  request("https://api.openweathermap.org/data/2.5/weather?q=Reutlingen,de&appid=08b92a35b3e616b4e4441a33deea7a9e",
+  function(error, response, body) {
+	  var parsedBody = JSON.parse(body);
+	  var temp = parsedBody["main"]["temp"];
+	  res.send({temp});
+})})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
